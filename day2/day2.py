96a5e9b7ed3723b4266bccdef883a524a7904eea
Hogan Bobertz <bobertzh@amazon.com>
2022-12-03 05:12:33 -0800

    day2

---
+++ day2/day2.py
@@ -0,0 +1,39 @@
+def part1():
+    """_summary_    A, X = Rock
+                    B, Y = Paper
+                    C, Z = Scissors
+                    win = 8, tie = 6, loss = 1
+    """
+    key = {'A X': 4,
+           'A Y': 8,
+           'A Z': 3,
+           'B X': 1,
+           'B Y': 5,
+           'B Z': 9,
+           'C X': 7,
+           'C Y': 2,
+           'C Z': 6}
+    sum = 0
+    input = open('input.txt','r')
+    for line in input.readlines():
+        sum+= key[line.strip()]
+    print(sum)
+
+def part2():
+    key = {'A X': 3,
+           'A Y': 4,
+           'A Z': 8,
+           'B X': 1,
+           'B Y': 5,
+           'B Z': 9,
+           'C X': 2,
+           'C Y': 6,
+           'C Z': 7}
+    sum = 0
+    input = open('input.txt','r')
+    for line in input.readlines():
+        sum+= key[line.strip()]
+    print(sum)
+
+part2()
+part1()
