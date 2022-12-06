cd106ab9d3082b2e09d49b66941ebba5b1cb680f
Hogan Bobertz <bobertzh@amazon.com>
2022-12-06 05:42:24 -0800

    day 6 part 2

--- day6.py
+++ day6.py
@@ -11,4 +11,13 @@ def part1(lines):
         else:
             sum+=1
             
-        
+@basic_boiler(fname='input6.txt')
+def part2(lines):
+    line = lines[0]
+    sum = 0
+    for i in range(len(line)-14):
+        if len(set(line[i:i+14])) == 14:
+            print(f'sum of {sum+14} at {line[i:i+4]}') 
+            break
+        else:
+            sum+=1
